7e06be456eef8628d45932f60b431cefd30507d5
Ltlyl-tk79 <taku.805740@gmail.com>
2021-05-28 06:25:34 +0000

    user/indexページ,showページの作成

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,2 +1,25 @@
 class BooksController < ApplicationController
+  
+  def index
+    @books = Book.all
+    @book = book.new
+    @user = current_user
+    
+  end
+  
+  def show
+  end
+  
+  def create
+  end
+  
+  def edit
+  end
+  
+  def update
+  end
+  
+  def destroy
+  end
+  
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,5 +7,8 @@ class UsersController < ApplicationController
   end
 
   def show
+    @user = User.find(params[:id])
+    @book = Book.new
+    @books = @user.books
   end
 end
