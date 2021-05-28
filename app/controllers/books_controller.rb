245317d6b96715a1fd8dec0c6faa9ee4fd5f382b
Ltlyl-tk79 <taku.805740@gmail.com>
2021-05-28 07:55:38 +0000

    book/indexページ,showページ作成

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,25 +1,41 @@
 class BooksController < ApplicationController
-  
+
   def index
     @books = Book.all
-    @book = book.new
+    @book = Book.new
     @user = current_user
-    
+
   end
-  
+
   def show
+    @book = Book.find(params[:id])
+    @booknew = Book.new
+    @user = @book.user
   end
-  
+
   def create
+    @book = Book.new(book_params)
+    @book.user_id = current_user.id
+    if @book.save
+      flash[:notice] = "You have created book successfully."
+      redirect_to book_path(@book.id)
+    else
+      render :index
+    end
   end
-  
+
   def edit
   end
-  
+
   def update
   end
-  
+
   def destroy
   end
-  
+
+  private
+  def book_params
+    params.require(:book).permit(:title, :body)
+  end
+
 end
