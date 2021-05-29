6a8509b73ef4afe3d279763b69157dcc14a40875
Ltlyl-tk79 <taku.805740@gmail.com>
2021-05-29 07:01:30 +0000

    book/editページ,user/editページ作成

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -11,4 +11,23 @@ class UsersController < ApplicationController
     @book = Book.new
     @books = @user.books
   end
+
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+    @user = User.find(params[:id])
+    if @user.update(user_params)
+      flash[:update] = "You have updated user successfully."
+      redirect_to user_path(@user.id)
+    else
+      render :edit
+    end
+  end
+
+  private
+  def user_params
+    params.require(:user).permit(:name, :introduction, :profile_image)
+  end
 end
--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,4 @@
 #     https: false
 #   )
 # end
+Refile.secret_key = '97a7b7e2faa43ad313d0167efede40c8bb914594b8eedc9dda15496a1ed4dce5208833ae2e3d7a460b468d8dd5da3042be0f392a64146cafcae1ce9adafbc69a'
